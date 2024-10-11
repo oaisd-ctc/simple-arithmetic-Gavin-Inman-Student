9f3a023e8743882ca302430c4ac4fe5349b75a10
Gavin-Inman-Student <gain6ab@student.oaisd.org>
2024-10-11 18:00:25 +0000

    Basic Long Calculator

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿Console.Clear();
+
+Console.WriteLine("Lets Add");
+Console.WriteLine("First Number");
+string Add1 = Console.ReadLine();
+Console.WriteLine("Second Number");
+string Add2= Console.ReadLine();
+float add1 = float.Parse(Add1);
+float add2 = float.Parse(Add2);
+Console.WriteLine(add1 + add2);
+
+Console.WriteLine("Lets Subtract");
+Console.WriteLine("First Number");
+string Sub1 = Console.ReadLine();
+Console.WriteLine("Second Number");
+string Sub2= Console.ReadLine();
+float sub1 = float.Parse(Sub1);
+float sub2 = float.Parse(Sub2);
+Console.WriteLine(sub1 - sub2);
+
+Console.WriteLine("Lets Multiply");
+Console.WriteLine("First Number");
+string Multi1 = Console.ReadLine();
+Console.WriteLine("Second Number");
+string Multi2= Console.ReadLine();
+float multi1 = float.Parse(Multi1);
+float multi2 = float.Parse(Multi2);
+Console.WriteLine(multi1 * multi2);
+
+Console.WriteLine("Lets Divide");
+Console.WriteLine("First Number");
+string Div1 = Console.ReadLine();
+Console.WriteLine("Second Number");
+string Div2= Console.ReadLine();
+float div1 = float.Parse(Div1);
+float div2 = float.Parse(Div2);
+Console.WriteLine(div1 / div2);
+
+Console.WriteLine("Modulus");
+Console.WriteLine("First Number");
+string Mod1 = Console.ReadLine();
+Console.WriteLine("Second Number");
+string Mod2= Console.ReadLine();
+int mod1 = int.Parse(Mod1);
+int mod2 = int.Parse(Mod2);
+Console.WriteLine(mod1 % mod2);
